677f4fbdace7249110abb313ba69ca6b8cec428c
WeversonSGusmao <weversongusmao365@gmail.com>
2024-06-26 00:28:27 -0300

    up

--- menu.js
+++ menu.js
@@ -1,11 +1,16 @@
 
 let btnMenu = document.getElementById('btn-menu')
 let menu = document.getElementById('menu-mobile')
+let overlay = document.getElementById('overlay-menu')
 
 btnMenu.addEventListener('click', ()=>{
   menu.ClassList.add('abrir-menu')
 })
 
 menu.addEventListener('click', ()=>{
+  menu.ClassList.remove('abrir-menu')
+})
+
+ooveerlay.addEventListener('click', ()=>{
   menu.ClassList.remove('abrir-menu')
 })
