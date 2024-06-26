80b9d13b1e901869d8f00c3f330028fe4094c9ce
Weverson Gusmão <weversongusmao365@gmail.com>
2024-06-25 23:34:41 -0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,3 +1,7 @@
 
 let btnMenu = document.getElementById('btn-menu')
 let menu = document.getElementById('menu-mobile')
+
+btnMenu.addEventListener('click', ()=>{
+  menu.ClassList.add('abrir-menu')
+})
