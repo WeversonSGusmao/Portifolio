a2a4f6ccb972a4ce00fa0769daca6296d5c090a4
WeversonSGusmao <weversongusmao365@gmail.com>
2024-07-06 21:19:34 -0300

    up

--- menu.js
+++ menu.js
@@ -16,10 +16,4 @@ overlay.addEventListener('click', ()=>{
 })
 
 
-document.getElementById('contatoForm').addEventListener('submit', function () {
-  var nome = this.querySelector('input[name=nome]'), nome = nome.value;
-  var email = this.querySelector('input[name=email]'), email = email.value;
-  var texto = 'Olá destinatário, \nMeu nome é '+ nome +' e meu email é '+ email;
-  this.querySelector('input[name=Body]').setAttribute('value', texto);
-});
 
